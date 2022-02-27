8c509dd29f880ccc185d3e2eaceb52ecf9e863e4
Roi Myw <samgrc.sat@gmail.com>
2022-02-27 15:46:18 +0100

    3.7 à commencer, j'attends le cours de mardi

--- src/ch/epfl/javelo/Functions.java
+++ src/ch/epfl/javelo/Functions.java
@@ -21,16 +21,27 @@ public final class Functions {
         }
     }
     private record Sampled(float[] samples, double xMax) implements DoubleUnaryOperator {
+
+        static int lowerBound = 0;
+
         public Sampled {
             if(samples.length < 2 || xMax <= 0) {
                 throw new IllegalArgumentException();
             }
         }
 
+        private int bound(double x) {
+            double intervalLength = xMax/samples.length;
+            while (x > xMax) {
+                x = x - intervalLength;
+                lowerBound++;
+            }
+            return lowerBound;
+        }
+
         @Override
         public double applyAsDouble(double x) {
-
-            return Math2.interpolate(samples[0], samples[1], x) ;
+            return Math2.interpolate(samples[bound(x)], samples[bound(x) + 1], x) ;
         }
     }
 }
