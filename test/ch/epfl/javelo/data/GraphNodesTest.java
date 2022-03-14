a8e1266277e6e50b0ff1744463d7349104cf9b4f
Roi Myw <samgrc.sat@gmail.com>
2022-03-14 10:03:13 +0100

    avancement étape 4

--- test/ch/epfl/javelo/data/GraphNodesTest.java
+++ test/ch/epfl/javelo/data/GraphNodesTest.java
@@ -1,24 +1,16 @@
 package ch.epfl.javelo.data;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.nio.IntBuffer;
 
+import static ch.epfl.test.TestRandomizer.RANDOM_ITERATIONS;
+import static ch.epfl.test.TestRandomizer.newRandom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-public class GraphNodesTest {
-
+class GraphNodesTest {
     @Test
-    public void countWorksOnNonTrivialArray() {
-        IntBuffer buffer = IntBuffer.wrap(new int[]{1, 2, 3});
-        GraphNodes graph = new GraphNodes(buffer);
-        double expected = 2;
-        assertEquals(expected, graph.nodeN(0), 0.001);
-    }
-
-    @Test
-    public void GraphNodesWorks() {
+    void graphNodesWorksOnGivenExample() {
         IntBuffer b = IntBuffer.wrap(new int[]{
                 2_600_000 << 4,
                 1_200_000 << 4,
@@ -33,8 +25,62 @@ public class GraphNodesTest {
         assertEquals(0x1235, ns.edgeId(0, 1));
     }
 
+    @Test
+    void graphNodesCountWorksFrom0To99() {
+        for (int count = 0; count < 100; count += 1) {
+            var buffer = IntBuffer.allocate(3 * count);
+            var graphNodes = new GraphNodes(buffer);
+            assertEquals(count, graphNodes.count());
+        }
+    }
 
-    public static void main(String[] args) {
+    @Test
+    void graphNodesENWorkOnRandomCoordinates() {
+        var nodesCount = 10_000;
+        var buffer = IntBuffer.allocate(3 * nodesCount);
+        var rng = newRandom();
+        for (int i = 0; i < RANDOM_ITERATIONS; i += 1) {
+            var e = 2_600_000 + 50_000 * rng.nextDouble();
+            var n = 1_200_000 + 50_000 * rng.nextDouble();
+            var e_q28_4 = (int) Math.scalb(e, 4);
+            var n_q28_4 = (int) Math.scalb(n, 4);
+            e = Math.scalb((double) e_q28_4, -4);
+            n = Math.scalb((double) n_q28_4, -4);
+            var nodeId = rng.nextInt(nodesCount);
+            buffer.put(3 * nodeId, e_q28_4);
+            buffer.put(3 * nodeId + 1, n_q28_4);
+            var graphNodes = new GraphNodes(buffer);
+            assertEquals(e, graphNodes.nodeE(nodeId));
+            assertEquals(n, graphNodes.nodeN(nodeId));
+        }
+    }
 
+    @Test
+    void graphNodesOutDegreeWorks() {
+        var nodesCount = 10_000;
+        var buffer = IntBuffer.allocate(3 * nodesCount);
+        var rng = newRandom();
+        for (int outDegree = 0; outDegree < 16; outDegree += 1) {
+            var firstEdgeId = rng.nextInt(1 << 28);
+            var nodeId = rng.nextInt(nodesCount);
+            buffer.put(3 * nodeId + 2, (outDegree << 28) | firstEdgeId);
+            var graphNodes = new GraphNodes(buffer);
+            assertEquals(outDegree, graphNodes.outDegree(nodeId));
+        }
+    }
+
+    @Test
+    void graphNodesEdgeIdWorksOnRandomValues() {
+        var nodesCount = 10_000;
+        var buffer = IntBuffer.allocate(3 * nodesCount);
+        var rng = newRandom();
+        for (int outDegree = 0; outDegree < 16; outDegree += 1) {
+            var firstEdgeId = rng.nextInt(1 << 28);
+            var nodeId = rng.nextInt(nodesCount);
+            buffer.put(3 * nodeId + 2, (outDegree << 28) | firstEdgeId);
+            var graphNodes = new GraphNodes(buffer);
+            for (int i = 0; i < outDegree; i += 1)
+                assertEquals(firstEdgeId + i, graphNodes.edgeId(nodeId, i));
+        }
     }
 }
