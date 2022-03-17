b06d277f176140a48d66591619bd5e3fb5915881
titi <74377782+titi1000@users.noreply.github.com>
2022-03-17 16:52:01 +0100

    tests step 4

---
+++ test/ch/epfl/javelo/routing/EdgeTest.java
@@ -0,0 +1,20 @@
+package ch.epfl.javelo.routing;
+
+import org.junit.jupiter.api.Test;
+
+public class EdgeTest {
+    @Test
+    void positionClosestToTest() {
+
+    }
+
+    @Test
+    void pointAtTest() {
+
+    }
+
+    @Test
+    void elevationAtTest() {
+
+    }
+}
---
+++ test/ch/epfl/javelo/routing/RoutePointTest.java
@@ -0,0 +1,15 @@
+package ch.epfl.javelo.routing;
+
+import org.junit.jupiter.api.Test;
+
+public class RoutePointTest {
+    @Test
+    void withPositionShiftedByTest() {
+
+    }
+
+    @Test
+    void minTest() {
+
+    }
+}
