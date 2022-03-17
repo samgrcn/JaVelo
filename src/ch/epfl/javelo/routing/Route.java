4782d2842ebf98c37aad4909926ce866ff9e303f
titi <74377782+titi1000@users.noreply.github.com>
2022-03-17 16:51:45 +0100

    step 4

---
+++ src/ch/epfl/javelo/routing/Route.java
@@ -0,0 +1,66 @@
+package ch.epfl.javelo.routing;
+
+import ch.epfl.javelo.projection.PointCh;
+
+import java.util.List;
+
+/**
+ * Represents a route.
+ *
+ * @author Quentin Chappuis (339517)
+ */
+public interface Route {
+
+    /**
+     * Returns the segment index at the given position (in metres).
+     * @param position the given position
+     * @return the segment index
+     */
+    int indexOfSegmentAt(double position);
+
+    /**
+     * Returns the length of the route, in metres.
+     * @return the length
+     */
+    double length();
+
+    /**
+     * Returns all the edges of the route.
+     * @return all the edges
+     */
+    List<Edge> edges();
+
+    /**
+     * Returns all the points located at the ends of the edges of the route.
+     * @return all the points
+     */
+    List<PointCh> points();
+
+    /**
+     * Returns the point at the given position along the route.
+     * @param position the given position
+     * @return the point
+     */
+    PointCh pointAt(double position);
+
+    /**
+     * Returns the altitude at the given position along the route.
+     * @param position the given position
+     * @return the altitude
+     */
+    double elevationAt(double position);
+
+    /**
+     * Returns the identity of the node belonging to the route and being closest to the given position.
+     * @param position the given position
+     * @return the identity of the closest node
+     */
+    int nodeClosestTo(double position);
+
+    /**
+     * Returns the point on the route that is closest to the given reference point.
+     * @param point the given point
+     * @return the closest point
+     */
+    RoutePoint pointClosestTo(PointCh point);
+}
---
+++ src/ch/epfl/javelo/routing/RoutePoint.java
@@ -0,0 +1,45 @@
+package ch.epfl.javelo.routing;
+
+import ch.epfl.javelo.projection.PointCh;
+
+/**
+ * Represents the point on a route closest to a given reference point, which is in the vicinity of the route.
+ *
+ * @author Quentin Chappuis (339517)
+ */
+public record RoutePoint(PointCh point, double position, double distanceToReference) {
+
+    public final static RoutePoint NONE = new RoutePoint(null, Double.NaN, Double.POSITIVE_INFINITY);
+
+    /**
+     * Retourne un point identique au récepteur (this) mais dont la position est décalée de la différence donnée, qui peut être positive ou négative.
+     * @param positionDifference the position difference that should be applied
+     * @return a new route point
+     */
+    public RoutePoint withPositionShiftedBy(double positionDifference) {
+        return new RoutePoint(this.point(), this.position() + positionDifference, this.distanceToReference());
+    }
+
+    /**
+     * Returns this if its distance to the reference is less than or equal to that, and that otherwise.
+     * @param that another route point
+     * @return this or that
+     */
+    public RoutePoint min(RoutePoint that) {
+        if (this.distanceToReference() <= that.distanceToReference()) return this;
+        return that;
+    }
+
+    /**
+     * Returns this if its distance to the reference is less than or equal to thatDistanceToReference,
+     * and a new instance of RoutePoint whose attributes are the arguments passed to min otherwise.
+     * @param thatPoint another route point
+     * @param thatPosition new position
+     * @param thatDistanceToReference the distance to compare
+     * @return this or a new route point
+     */
+    public RoutePoint min(PointCh thatPoint, double thatPosition, double thatDistanceToReference) {
+        if (this.distanceToReference() <= thatDistanceToReference) return this;
+        return new RoutePoint(thatPoint, position, thatDistanceToReference);
+    }
+}
