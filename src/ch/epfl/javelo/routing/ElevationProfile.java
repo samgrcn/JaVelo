80ded6155a317cf2e4b4e1b9cd65edc5988b7602
Roi Myw <samgrc.sat@gmail.com>
2022-03-19 10:14:46 +0100

    Partie Samuel testé & commentée

--- src/ch/epfl/javelo/routing/ElevationProfile.java
+++ src/ch/epfl/javelo/routing/ElevationProfile.java
@@ -14,13 +14,26 @@ public class ElevationProfile {
     private final float[] elevationSamples;
     private final DoubleUnaryOperator function;
 
+    /**
+     * Constructs the long profile of a route of length (in meters) and whose elevation samples,
+     * uniformly distributed along the route, are contained in elevationSamples.
+     * @param length length of the route (in meters)
+     * @param elevationSamples elevations of the profile
+     * @throws IllegalArgumentException if the length is negative or null,
+     * or if the sample array contains less than 2 elements
+     */
+
     public ElevationProfile(double length, float[] elevationSamples) {
         this.function = Functions.sampled(elevationSamples, length);
         this.length = length;
         this.elevationSamples = Arrays.copyOf(elevationSamples, elevationSamples.length);
     }
 
-
+    /**
+     *
+     * @param elevationSamples
+     * @return
+     */
 
     private double maxValues(float[] elevationSamples) {
         DoubleSummaryStatistics statistics = new DoubleSummaryStatistics();
@@ -38,11 +51,31 @@ public class ElevationProfile {
         return statistics.getMin();
     }
 
+    /**
+     * Gives the length of the profile (in meters).
+     * @return length of the profile (in meters)
+     */
+
     public double length() { return length; }
+
+    /**
+     * Gives  the minimum altitude of the profile (in meters).
+     * @return the minimum altitude of the profile (in meters)
+     */
 
     public double minElevation() { return minValues(elevationSamples); }
 
+    /**
+     * Gives the maximum altitude of the profile (in meters).
+     * @return returns the maximum altitude of the profile (in meters)
+     */
+
     public double maxElevation() { return maxValues(elevationSamples); }
+
+    /**
+     * Gives the total positive vertical drop of the profile (in meters).
+     * @return the total positive vertical drop of the profile (in meters)
+     */
 
     public double totalAscent() {
         float totalAscent = 0;
@@ -55,6 +88,11 @@ public class ElevationProfile {
         return totalAscent;
     }
 
+    /**
+     * Gives the total negative elevation of the profile (in meters).
+     * @return the total negative elevation of the profile (in meters)
+     */
+
     public double totalDescent() {
         float totalDescent = 0;
         for (int i = 0; i < elevationSamples.length - 1; i++) {
@@ -64,6 +102,12 @@ public class ElevationProfile {
         }
         return totalDescent ;
     }
+
+    /**
+     * Gives the altitude of the profile at the given position.
+     * @param position desired position to get the altitude.
+     * @return the altitude of the profile at the position
+     */
 
     public double elevationAt(double position) { return function.applyAsDouble(position); }
 
