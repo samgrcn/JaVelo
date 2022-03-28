361f28f2cad64d98e97e46379706d33fe1593a99
Roi Myw <samgrc.sat@gmail.com>
2022-03-28 22:09:14 +0200

    3.4 fini

--- src/ch/epfl/javelo/routing/RouteComputer.java
+++ src/ch/epfl/javelo/routing/RouteComputer.java
@@ -5,6 +5,7 @@ import ch.epfl.javelo.data.Graph;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class RouteComputer {
 
@@ -17,22 +18,32 @@ public class RouteComputer {
     }
 
     public Route bestRouteBetween(int startNodeId, int endNodeId) {
+
+        record WeightedNode(int nodeId, float distance)
+                implements Comparable<WeightedNode> {
+            @Override
+            public int compareTo(WeightedNode that) {
+                return Float.compare(this.distance, that.distance);
+            }
+        }
+
         Preconditions.checkArgument(startNodeId != endNodeId);
-        double[] distance = new double[graph.nodeCount()];
-        List<Integer> exploration = new ArrayList<>();
+        float[] distance = new float[graph.nodeCount()];
+        int[] predecessor = new int[graph.nodeCount()];
+        PriorityQueue<WeightedNode> exploration = new PriorityQueue<>();
+        List<Integer> pathNode = new ArrayList<>();
         for (int i = 0; i < graph.nodeCount(); i++) {
             distance[i] = Float.POSITIVE_INFINITY;
+            predecessor[i] = 0;
         }
         distance[startNodeId] = 0;
-        exploration.add(startNodeId);
-        int n = exploration.get(0);
+        exploration.add(new WeightedNode(startNodeId, distance[startNodeId]));
+        int n;
         while (!exploration.isEmpty()) {
-            for (Integer i : exploration) {
-                if (distance[n] < distance[i]) {
-                    n = i;
-                }
-            }
-            exploration.remove(n);
+            n = exploration.peek().nodeId;
+            exploration.remove();
+            distance[n] = Float.NEGATIVE_INFINITY;
+        pathNode.add(n);
             if(n == endNodeId) {
                 break;
             }
@@ -40,15 +51,34 @@ public class RouteComputer {
             int n2;
             for (int i = 0; i < numberOfEdges; i++) {
                 for (int j = graph.nodeOutEdgeId(n, 0); j < graph.nodeOutEdgeId(n, numberOfEdges); j++) {
+                    double costFactor = costFunction.costFactor(n, j);
                     n2 = graph.edgeTargetNodeId(j);
-                    double d = distance[n] + graph.edgeLength(j);
+                    float d = (float)((distance[n] + graph.edgeLength(j))*(costFactor));
                     if(d < distance[n2]) {
                         distance[n2] = d;
-                        exploration.add(n2);
+                        predecessor[n2] = n;
+                        exploration.add(new WeightedNode(n2, distance[n2]));
                     }
                 }
             }
         }
+
+        List<Edge> routeEdgeList = new ArrayList<>();
+        int currentNodeId = endNodeId;
+        int previousNodeId = predecessor[endNodeId];
+        int edgeId;
+        while(currentNodeId != startNodeId) {
+            for (int i = 0; i < graph.nodeOutDegree(previousNodeId); i++) {
+                edgeId = graph.nodeOutEdgeId(previousNodeId, i);
+                if (graph.edgeTargetNodeId(edgeId) == currentNodeId); {
+                    routeEdgeList.add(new Edge(previousNodeId, currentNodeId, graph.nodePoint(previousNodeId), graph.nodePoint(currentNodeId), graph.edgeLength(edgeId), graph.edgeProfile(edgeId)));
+                }
+                currentNodeId = previousNodeId;
+                previousNodeId = predecessor[currentNodeId];
+            }
+
+        }
+        return new SingleRoute(routeEdgeList);
     }
 
 
