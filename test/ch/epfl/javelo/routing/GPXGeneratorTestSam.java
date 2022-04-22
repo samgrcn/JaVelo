774ec82bd420708d444b6ec4c17e4d05c50f7643
samgr <samgrc.sat@gmail.com>
2022-04-22 12:39:04 +0200

    étape 7 finie partie samuel + correction

--- test/ch/epfl/javelo/routing/GPXGeneratorTestSam.java
+++ test/ch/epfl/javelo/routing/GPXGeneratorTestSam.java
@@ -13,6 +13,6 @@ public class GPXGeneratorTestSam {
         Route r = rc.bestRouteBetween(159049, 117669);
         ElevationProfile e = ElevationProfileComputer.elevationProfile(r, 1);
 
-        GpxGenerator.writeGpx("ouii2.gpx", r, e);
+        GpxGenerator.writeGpx("test.gpx", r, e);
     }
 }
